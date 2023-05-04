c19a188d37efaf00ce57e3c1c639afc7f46726bd
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-05-04 17:22:40 +0300

    add db connection code

---
+++ db/connect.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const MONGO_URI =
+    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@google-photos.yuwz1gi.mongodb.net/?retryWrites=true&w=majority`
+
+const PORT = process.env.PORT || 8080;
+
+module.exports = (app) => {
+    mongoose.connect(MONGO_URI)
+        .then(() => {
+            app.listen(PORT, () => {
+                console.log(`Server is running on localhost:${PORT} ✨`);
+            });
+        })
+        .catch((err) => {
+            console.error(err)
+            console.log("Something went wrong with the database connection");
+        });
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const middlewares = require('./middlewares');
 const routes = require('./routes');
+const connectToMongoDB_andStartTheServer = require('./db/connect');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,7 +11,4 @@ const PORT = process.env.PORT || 8080;
 middlewares(app);
 routes(app);
 
-
-app.listen(PORT, () => {
-    console.log(`server now is live on port ${PORT} ✨`)
-})
+connectToMongoDB_andStartTheServer(app)
