219741cf452cab1deb318fe4ce780fa98e2e9ee8
mo7mad4 <mohammedreda814@gmail.com>
2023-05-05 20:52:07 +0300

    update some code

--- controllers/profile/index.js
+++ controllers/profile/index.js
@@ -1,9 +1,9 @@
 const User = require("../../db/Schemas/user");
-const auth = require("../auth");
-const token = require("../../db/Schemas/token");
+const bcrypt = require("bcryptjs");
+
+
 
 // ! CHECK THE REQUIREMENT DOCUMENT TO KNOW THE REQUEST AND RESPONSE SCHEMAS
-
 
 const getProfile = async (req, res, next) => {
   // TODO: get user profile using user_id
@@ -16,25 +16,17 @@ const getProfile = async (req, res, next) => {
   next();
 };
 
-
-
-
-
 const deleteProfile = async (req, res, next) => {
   // TODO: delete user profile using user_id
   const user = await User.findById(req.params.id).select("-password");
   if (user) {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json({message:"user has been delete successfully"});
+    res.status(200).json({ message: "user has been delete successfully" });
   } else {
     res.status(404).json({ message: "user not found" });
   }
   next();
 };
-
-
-
-
 
 const updateProfile = async (req, res, next) => {
   // TODO: update user profile using user_id
@@ -52,9 +44,8 @@ const updateProfile = async (req, res, next) => {
     req.params.id,
     {
       $set: {
-        email: req.body.email,
-        password: req.body.password,
         username: req.body.username,
+        password: req.body.password,
       },
     },
     { new: true }
