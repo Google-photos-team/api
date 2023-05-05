87c66ff181d56fee87f4a8dd5062f6883f56d868
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-05-05 23:52:52 +0300

    add folders end points controller and create image
    #7

--- controllers/images/index.js
+++ controllers/images/index.js
@@ -1,7 +1,81 @@
 // ! CHECK THE REQUIREMENT DOCUMENT TO KNOW THE REQUEST AND RESPONSE SCHEMAS
+const mongoose = require('mongoose');
+const RESPONSE_STATUS = require('../../constants/status');
+const Image = require('../../db/Schemas/image');
+const Folder = require('../../db/Schemas/folder');
+const User = require('../../db/Schemas/user');
 
-const createImage = (req, res, next) => {
+const { imageSchema } = require('../../utils/validation');
+
+const createImage = async (req, res, next) => {
     // TODO: create a new image and save it with the user preferred upload quality using sharp
+    try {
+        const { user_id } = req;
+        const { folder_id, name, tags, image } = req.body;
+        await imageSchema.validate({
+            folder_id, name, tags, image
+        }, { abortEarly: false })
+        if (!mongoose.Types.ObjectId.isValid(folder_id)) {
+            return res.status(400).send({
+                status: RESPONSE_STATUS.FAILED,
+                error: "invalid folder id"
+            })
+        }
+
+        const folder = await Folder.findOne({ user_id, _id: folder_id });
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(404).json({
+                status: RESPONSE_STATUS.FAILED,
+                error: "user id not exist in the database"
+            })
+        }
+
+        if (!folder) {
+            return res.status(404).json({
+                status: RESPONSE_STATUS.FAILED,
+                error: "folder id not exist in the database"
+            })
+        }
+
+        const imageInstance = await Image.create({
+            folder_id,
+            name,
+            tags,
+            image
+        })
+
+        folder.images = [...folder.images, imageInstance._id];
+        user.images = [...user.images, imageInstance._id];
+
+        await folder.save();
+        await user.save();
+
+        return res.status(200).json({
+            status: RESPONSE_STATUS.SUCCESS,
+            data: imageInstance
+        })
+
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            const errs = {};
+            error.inner.forEach(({ message, params }) => {
+                errs[params.path] = message;
+            });
+
+            return res.status(400).json({
+                status: RESPONSE_STATUS.FAILED,
+                errors: errs,
+            })
+        } else {
+            console.log({ error })
+            return res.status(500).send({
+                status: RESPONSE_STATUS.FAILED,
+                error: "something went wrong"
+            })
+        }
+    }
 }
 
 const deleteImages = (req, res, next) => {
--- utils/validation/index.js
+++ utils/validation/index.js
@@ -14,6 +14,14 @@ const signupSchema = yup.object({
     .required(),
 })
 
+const imageSchema = yup.object({
+  folder_id: yup.string().required("Folder ID is required"),
+  name: yup.string().required("Image Name is required"),
+  image: yup.string().required("Image is required"),
+  tags: yup.array().of(yup.string()).required("Image tags field is required"),
+})
+
 module.exports = {
-  signupSchema
+  signupSchema,
+  imageSchema
 }
