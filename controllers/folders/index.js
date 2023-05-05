7e1bcf079cf4572042d24aedb06a09f726099beb
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-05-05 19:17:00 +0300

    fix conflict

--- controllers/folders/index.js
+++ controllers/folders/index.js
@@ -5,10 +5,11 @@ const Folder = require('../../db/Schemas/folder');
 const getFolders = (req, res, next) => {
     // TODO: get all user folders from the db
     try {
-        const { user_id } = req.headers;
+        const { user_id } = req;
+        console.log({ user_id })
         const folders = Folder.find({ user_id: user_id });
-        console.log(folders);
-        res.status(200).json(folders)
+        // console.log(folders);
+        res.status(200).json({ folders })
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: "something went wrong" })
