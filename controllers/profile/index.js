cc4a6f355a633e41aa900b1feb88022a4fefff1c
mo7mad4 <mohammedreda814@gmail.com>
2023-05-10 17:06:57 +0300

    update code for profile controllers and route

--- controllers/profile/index.js
+++ controllers/profile/index.js
@@ -1,58 +1,62 @@
-const { RESPONSE_STATUS_USER } = require("../../constants/status");
+const { RESPONSE_STATUS_USER, RESPONSE_STATUS } = require("../../constants/status");
+const image = require("../../db/Schemas/image");
 const User = require("../../db/Schemas/user");
 const bcrypt = require("bcryptjs");
 
 // ! CHECK THE REQUIREMENT DOCUMENT TO KNOW THE REQUEST AND RESPONSE SCHEMAS
+
+// @desc      get user 
+// @route     GET /profile
+// @access    public
+
 //getProfile
 const getProfile = async (req, res, next) => {
   // TODO: get user profile using user_id
   try {
     const { user_id } = req;
-    const user = await User.findById(user_id).select("-password").populate({path:"profile"});
-    res.status(200)
-      .json({
-        status:RESPONSE_STATUS_USER.SUCCESS,
-      }).end()
-    if (user) {
-      return res.status(200).json(user);
-    } else {
-      return res.status(404).json({ message: "user not found" });
-    }
-  } catch (error) {
-      console.log(error)
-      res.status(500).send({message:"something went wrong"})
-  }
+    const user = await User.findById(user_id);
+    res.status(200).json({
+      status: RESPONSE_STATUS.SUCCESS,
+      username: user.username,
+      avatar: user.avatar,
+      images:user.images
+    }).end()
+} catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "something went wrong" })
+}
 };
+
+
+// @desc      delete user 
+// @route     DELETE /profile/delete
+// @access    public
 
 //deleteProfile
 const deleteProfile = async (req, res, next) => {
   // TODO: delete user profile using user_id
-
   try {
-    const { user_id } = req;
-    console.log(user_id);
-    const user = await User.findById(user_id).select("-password");
-    if (user) {
-      console.log("exist");
-      await User.findByIdAndDelete(user_id);
-      return res
-        .status(200)
-        .json({ message: "user has been delete successfully" });
-    } else {
-      console.log("not exist");
-      return res.status(404).json({ message: "user not found" });
-    }
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({message:"something went wrong"})  }
+    await User.findOneAndRemove({ _id: req.user_id});
+    res.json({
+      status: RESPONSE_STATUS.SUCCESS, 
+      msg: 'User profile deleted', 
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
 };
+
+// @desc      Update user data 
+// @route     post /api/v1/users/:id
+// @access    public
 
 //updateProfile
 const updateProfile = async (req, res, next) => {
-  try {
     // TODO: update user profile using user_id
+  try {
     const { user_id } = req;
-    const { error } = validateUpdateUser(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
@@ -66,18 +70,24 @@ const updateProfile = async (req, res, next) => {
       user_id,
       {
         $set: {
+          status: RESPONSE_STATUS.SUCCESS,
           username: req.body.username,
           password: req.body.password,
+          avatar: req.body.avatar,
+          images:req.body,image
         },
       },
       { new: true }
-    ).select("-password");
+    )
 
     return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error)
-    res.status(500).send({message:"something went wrong"})
-  }
+    res.status(500).send({
+      status: RESPONSE_STATUS.FAILED,
+      message:"something went wrong"
+    })
+  }  
 };
 
 module.exports = {
