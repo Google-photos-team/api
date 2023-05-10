d45df57f0da731b70d689b65774d1841d98df540
mo7mad4 <mohammedreda814@gmail.com>
2023-05-10 17:04:32 +0300

    Update

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-// const verifyToken = require('../../helpers/verifyToken');
+
 // controllers
 const { ProfileController } = require('../../controllers');
 
-router.delete('user/delete', ProfileController.deleteProfile)
+router.delete('/user/delete' ,ProfileController.deleteProfile)
 
-router.post('user/update', ProfileController.updateProfile)
+router.post('/user/update', ProfileController.updateProfile)
 
-router.get('user/:id', ProfileController.getProfile)
+router.get('/user/', ProfileController.getProfile)
 
 module.exports = router
